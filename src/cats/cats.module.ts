56df95286dc279234a546d44af3165540e2a69f7
may <bankouzhenyou8@gmail.com>
2025-01-07 15:18:03 +0900

    prisma設定

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CatsController } from './cats.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [CatsService],
   controllers: [CatsController],
 })
